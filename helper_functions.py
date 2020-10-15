82dac41fa0bca6aab2683e7c4e0f9c206c93c9d5
richie312 <richie.chatterjee31@gmail.com>
2020-10-15 12:24:55 +0530

    added the changes for trnsaction and helper_functions.py for rules

--- helper_functions.py
+++ helper_functions.py
@@ -179,7 +179,7 @@ def rules_check(conn):
     result = {}
     day_of_month = datetime.now().day
     week_number = (day_of_month - 1) // 7 + 1
-    if new_df['actual_quantity'][new_df['items'] == 'rum'].values[0] ==  new_df['planned_quantity'][new_df['items'] == 'rum'].values[0]/week_number:
+    if new_df['actual_quantity'][new_df['items'] == 'rum'].values[0] ==  week_number * 2:
         Color='#FFA500'
         Quantity=new_df['actual_quantity'][new_df['items'] == 'rum'].values[0]
 
@@ -258,17 +258,17 @@ def rules_check(conn):
     day_of_month = datetime.now().day
     week_number = (day_of_month - 1) // 7 + 1
 
-    if new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] == new_df['estimated_price'][new_df['items'] == 'chicken'].values[0] * week_number:
+    if new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] == new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] / week_number:
         Color='#FFA500'
         Quantity=new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0]
 
-    elif new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] < new_df['estimated_price'][new_df['items'] == 'chicken'].values[0] * week_number:
+    elif new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] < new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] / week_number:
         Color = '#008000'
-        Quantity = new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] * week_number - new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0]
+        Quantity = new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] / week_number - new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0]
 
-    elif new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0]  > new_df['estimated_price'][new_df['items'] == 'chicken'].values[0] * week_number:
+    elif new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0]  > new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] / week_number:
         Color = '#FF0000'
-        Quantity = new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] - new_df['estimated_price'][new_df['items'] == 'chicken'].values[0] * week_number
+        Quantity = new_df['actual_total_cost'][new_df['items'] == 'chicken'].values[0] - new_df['estimated_total_cost'][new_df['items'] == 'chicken'].values[0] / week_number
     result['chicken'] = {'Color':Color,'Quantity': Quantity}
 
 
@@ -317,11 +317,11 @@ def rules_check(conn):
 
     elif new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] < 250 * week_number:
         Color = '#008000'
-        Quantity = new_df['estimated_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] / week_number - new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0]
+        Quantity = (new_df['estimated_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] / week_number)* week_number - new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0]
 
     elif new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0]  > 250 * week_number:
         Color = '#FF0000'
-        Quantity = new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] - new_df['estimated_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] / week_number
+        Quantity = new_df['actual_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] - (new_df['estimated_total_cost'][new_df['items'] == 'Miscellaneous'].values[0] / week_number)*week_number
     result['Miscellaneous'] = {'Color':Color,'Quantity': Quantity}
 
     return result
